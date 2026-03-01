e2994f080f748b6a91e44644e88d119be5cdb8df
javruifer1 <forjavi22@gmail.com>
2026-03-02 00:34:25 +0100

    Revert "v3 boton borrar"
    This reverts commit 8353cfbe74fb21f2a73a9a2303cc2f76602e8144.

--- backend/current/middleware.py
+++ backend/current/middleware.py
@@ -4,34 +4,17 @@ class CorsMiddleware(MiddlewareMixin):
     def process_response(self, request, response):
         origin = request.headers.get('Origin')
 
-        allowed_origins = {
-            "http://localhost:8081",
-            "http://127.0.0.1:8081",
-            "http://localhost:19006",
-            "http://127.0.0.1:19006",
-            "http://localhost:3000",
-            "http://127.0.0.1:3000",
-        }
+        allowed_origins = [
+            "http://localhost:8081"
+        ]
 
-        is_localhost_origin = (
-            isinstance(origin, str)
-            and (
-                origin.startswith("http://localhost:")
-                or origin.startswith("http://127.0.0.1:")
-            )
-        )
-
-        if origin in allowed_origins or is_localhost_origin:
+        if origin in allowed_origins:
             response["Access-Control-Allow-Origin"] = origin
             response["Access-Control-Allow-Credentials"] = "true"
-            response["Vary"] = "Origin"
 
-        response.setdefault(
-            "Access-Control-Allow-Methods",
-            "GET, POST, PUT, PATCH, DELETE, OPTIONS",
-        )
-        response.setdefault(
-            "Access-Control-Allow-Headers",
-            "Content-Type, Authorization, X-CSRFToken, X-Requested-With",
-        )
+        else:
+            response["Access-Control-Allow-Origin"] = "*"
+        
+        response.setdefault("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+        response.setdefault("Access-Control-Allow-Headers", "Content-Type, Authorization")
         return response
