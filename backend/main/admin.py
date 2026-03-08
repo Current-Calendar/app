b65ed2f55ed8607d76c282e074332fd82347c674
LuisGomezdeTerreros <gomezyoramas@gmail.com>
2026-03-08 12:49:35 +0100

    feat: Add admin panel

--- backend/main/admin.py
+++ backend/main/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.gis.admin import GISModelAdmin
+from .models import Usuario, Calendario, Evento
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+@admin.register(Calendario)
+class CalendarioAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'creador', 'estado', 'origen', 'fecha_creacion')
+    list_filter  = ('estado', 'origen')
+    search_fields = ('nombre', 'creador__username')
+
+@admin.register(Evento)
+class EventoAdmin(GISModelAdmin):
+    list_display = ('titulo', 'creador', 'nombre_lugar', 'fecha_creacion', 'fecha')
+    search_fields = ('titulo', 'creador__username')
+    filter_horizontal = ('calendarios',) 
+@admin.register(Usuario)
+class UsuarioAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('Perfil', {'fields': ('pronombres', 'biografia', 'link', 'foto')}),
+        ('Social',  {'fields': ('seguidos', 'calendarios_seguidos')}),
+    )
+    filter_horizontal = ('seguidos', 'calendarios_seguidos') 
+
+    list_display = ('username', 'email', 'is_staff', 'total_seguidores')    
