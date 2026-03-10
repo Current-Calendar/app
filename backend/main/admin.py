f3af9457c121470e98221498571ed777413dee33
AlvaroBarragan <trabajoalvaro1909@gmail.com>
2026-03-10 01:19:51 +0100

    fix name

--- backend/main/admin.py
+++ backend/main/admin.py
@@ -1,20 +1,20 @@
 from django.contrib import admin
 from django.contrib.gis.admin import GISModelAdmin
-from .models import Usuario, Calendario, Evento
+from .models import User, Calendar, Event
 from django.contrib.auth.admin import UserAdmin
 
-@admin.register(Calendario)
+@admin.register(Calendar)
 class CalendarioAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'creador', 'estado', 'origen', 'fecha_creacion')
     list_filter  = ('estado', 'origen')
     search_fields = ('nombre', 'creador__username')
 
-@admin.register(Evento)
+@admin.register(Event)
 class EventoAdmin(GISModelAdmin):
     list_display = ('titulo', 'creador', 'nombre_lugar', 'fecha_creacion', 'fecha')
     search_fields = ('titulo', 'creador__username')
     filter_horizontal = ('calendarios',) 
-@admin.register(Usuario)
+@admin.register(Event)
 class UsuarioAdmin(UserAdmin):
     fieldsets = UserAdmin.fieldsets + (
         ('Perfil', {'fields': ('pronombres', 'biografia', 'link', 'foto')}),
