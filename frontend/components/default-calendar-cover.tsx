f4b076f452641009f482cdfa224882c8873c2b58
Jesús Villalba <jvilfer1712@gmail.com>
2026-04-12 19:30:18 +0200

    feat: add help and support in settings

--- frontend/components/default-calendar-cover.tsx
+++ frontend/components/default-calendar-cover.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+git pimport React from "react";
 import { View, Text, StyleSheet, type StyleProp, type ViewStyle } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
