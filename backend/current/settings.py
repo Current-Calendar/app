f9b7d5c03a1119508a34819a21a2693893b7f0cf
Alejandro Parody <alejandroparodyq@gmail.com>
2026-05-04 21:14:44 +0200

    fix: added jwt middleware for graphsql calls

--- backend/current/settings.py
+++ backend/current/settings.py
@@ -166,6 +166,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'current.middleware.JWTAuthMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
