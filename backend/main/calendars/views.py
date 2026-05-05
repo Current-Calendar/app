991fd54a96aa8675f65eadd2ac1066f64833c829
Manuel Alcaraz Zambrano <manalczam@alum.us.es>
2026-05-05 20:15:32 +0200

    fix: don't allow view invites to coowners
    If the user is already a coowner it can already view the calendar.
    
    Refs #479

--- backend/main/calendars/views.py
+++ backend/main/calendars/views.py
@@ -1183,7 +1183,7 @@ def invite_calendar(request: Request, calendar_id: int) -> Response:
             status=status.HTTP_400_BAD_REQUEST,
         )
 
-    if invite_type == "EDIT" and calendar.co_owners.filter(id=user_to_invite.id).exists():
+    if calendar.co_owners.filter(id=user_to_invite.id).exists():
         return Response(
             {"error": "User is already a co-owner of the calendar"},
             status=status.HTTP_400_BAD_REQUEST,
